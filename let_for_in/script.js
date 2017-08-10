d7fa361051fbecc00dc6120cf58f6c67917043a7
oduvanio <oduvanio@mail.ru>
2017-08-10 10:02:50 +0400

    Create script.js

---
+++ let_for_in/script.js
@@ -0,0 +1,14 @@
+var funcs = [],
+object = {
+	a: true,
+	b: true,
+	c: true
+};
+for (let key in object) {
+	funcs.push(function() {
+		console.log(key);
+	});
+}
+funcs.forEach(function(func) {
+	func();
+})

6e2eb54f2922914c642ff2fc02b14c01e19437a6
oduvanio <oduvanio@mail.ru>
2017-08-12 15:03:28 +0400

    Create script.js

---
+++ super/script.js
@@ -0,0 +1,14 @@
+var o1 = {
+	foo(){
+		console.log( "o1:foo" );
+	}
+};
+var o2 = {
+	foo() {
+		super.foo();
+		console.log( "o2:foo" );
+	}
+};
+Object.setPrototypeOf( o2, o1 );
+o2.foo(); // o1:foo
+	  // o2:foo
